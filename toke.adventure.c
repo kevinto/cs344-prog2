40eb8d6a615cd206408753856ead0d26aaf6e659
kevinto <kevinto1989@gmail.com>
2015-04-21 08:58:12 -0700

    added code to create file directories

--- toke.adventure.c
+++ toke.adventure.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+struct stat st = {0};
 
 int main()
 {
+   // Get Process Id
    int curPid = getpid();
    char pidString[10];
    snprintf(pidString, sizeof(pidString), "%d", curPid);
 
+   // Generate rooms directory name
    char directoryName[] = "toke.rooms.";
    strcat(directoryName, pidString);
-   printf("dir name: %s\n", directoryName);
+
+   // Create rooms directory
+   if (stat(directoryName, &st) == -1)
+   {
+      mkdir(directoryName, 0700);
+   }
 }
